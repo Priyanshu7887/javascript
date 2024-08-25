51bd2a59eaf8a4321c32477faca788137cbe9c2d
Priyanshu Dubey <131892487+Priyanshu7887@users.noreply.github.com>
2024-08-25 13:41:26 +0000

    typeconversion

---
+++ Basics/Type_conversion.js
@@ -0,0 +1,14 @@
+//convert from one datatype to another type
+let score = "33A"
+console.log(typeof score);
+let newscore = Number(score);
+console.log(newscore);
+console.log(typeof newscore);
+
+//convert from number to boolean
+
+let Isloggined = 1
+console.log(typeof Isloggined);
+let newlogin = Boolean(Isloggined);
+console.log(newlogin);
+console.log(typeof newlogin);
