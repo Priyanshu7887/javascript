4b19900f7b500516a2ac3eb2dffab50f34d7d02a
Priyanshu Dubey <131892487+Priyanshu7887@users.noreply.github.com>
2024-08-25 05:41:09 +0000

    datatypes

--- Basics/Datatypes.js
+++ Basics/Datatypes.js
@@ -1,1 +1,15 @@
 "use strict" //compilar treat as a newer version of js
+
+let name="priyanshu"//string datatype value should be in double quotes ""
+let age = 21 //number datatype
+let isgranted = true // boolean datatype only yes or no
+let marks; // undefined which means value is not assigned initially
+
+
+//nan(Not A Number)
+//null(standalone value)
+
+console.log( typeof(name))
+console.log (typeof(age))
+console.log (typeof(isgranted))
+console.log (typeof(marks))
